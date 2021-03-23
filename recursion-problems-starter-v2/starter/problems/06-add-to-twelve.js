f0bbc468e1268b816f2ae851ffef4425c3e86899
Andru <dr3wwatkins@gmail.com>
2021-03-23 18:11:23 -0500

    solved problem 6

--- recursion-problems-starter-v2/starter/problems/06-add-to-twelve.js
+++ recursion-problems-starter-v2/starter/problems/06-add-to-twelve.js
@@ -13,14 +13,24 @@ addToTwelve([1]); // false
 ***********************************************************************/
 
 function addToTwelve(array){
-  let total = 0;
-  
+
+  let total = array[0] + array[1];
+
   if(total === 12)
     return true;
-  total = array[0] + array[1];
+    if (array.length <= 2)
+    return false;
   //array[0] + array[1] //add 1 to each of them until the second value is the array length
 
+  return  addToTwelve(array.slice(1));
+  
 }
+
+console.log(addToTwelve([1, 3, 4, 7, 5])); // true
+// addToTwelve([1, 3, 4, 7, 6]); // false
+// addToTwelve([1, 11, 4, 7, 6]); // true
+// addToTwelve([1, 12, 4, 7, 6]); // false
+console.log(addToTwelve([1])); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
--- recursion-problems-starter-v2/starter/problems/07-exponent.js
+++ recursion-problems-starter-v2/starter/problems/07-exponent.js
@@ -1,5 +1,5 @@
 /***********************************************************************
-Write a recursive function called `exponent` that takes two integers, 
+Write a recursive function called `exponent` that takes two integers,
 `num` and `power`, and returns `num` raised to the `power`th power. Your
 function should work when `num` or `power` are positive OR negative.
 
@@ -21,9 +21,24 @@ exponent(5, 5); // 3125
 ***********************************************************************/
 
 function exponent(b, n) {
-  // your code here
+  if (n === 1 || n === 0) {
+    //console.log(`We've reached the base case of 0! Let's start going back up the stack.`)
+    return b;
+
+  } else if (n > 1) {
+
+    return b*exponent(b, n -1);
+
+  } else if (n < -1) {
+    return b/exponent(b, n +1)
+
+  }
 }
-  
+console.log(exponent(3, 2)); // 9
+console.log(exponent(2, -2)); // 1/4 (or 0.25)
+console.log(exponent(5, 5)); // 3125
+
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = exponent;
