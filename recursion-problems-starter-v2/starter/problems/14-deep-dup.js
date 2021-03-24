385903e7ec88f08fd5ab61c9dee7cc3410fc80ec
Andru <dr3wwatkins@gmail.com>
2021-03-24 15:42:46 -0500

    notes

--- recursion-problems-starter-v2/starter/problems/14-deep-dup.js
+++ recursion-problems-starter-v2/starter/problems/14-deep-dup.js
@@ -14,7 +14,7 @@ since we used 'slice' in the example above that 'slice' creates a shallow copy, 
 correct. For this problem we duplicate our original array so that our function returns a new
 array with different memory adresses.
 
-Write a function, deepDup(arr), that deeply duplicates a given array. Your duplicated array, 
+Write a function, deepDup(arr), that deeply duplicates a given array. Your duplicated array,
 when compared to various indexes of the original array, should evaluate to false like below.
 
 
@@ -35,8 +35,12 @@ console.log(x[0] === y[0]) // true
 
 
 ***********************************************************************/
+let x = [1, 2, 3];
+debugger
+let y = x.slice();
+debugger
+console.log(x[0] === y[0]) // true
 
-// your code here
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
